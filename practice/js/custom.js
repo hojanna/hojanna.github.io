3739b3047102db995692f47d33c535e6b201786f
hojanna <seungho2528@naver.com>
2024-01-17 16:31:42 +0900

    수정본

---
+++ practice/js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  /* brunch news */
+  $('.brunch_news.sliders').slick({
+    arrows:false,
+    dots:false,
+    infinite:true,
+    autoplay:true,
+    autoplaySpeed:2000,
+    vertical:true,
+    verticalSwiping:true,
+  })
+  /* brunch recommend */
+  // $('.brunch_recommend.sliders').slick({
+  //   dots:true,
+  //   infinite:false,
+  //   slidesToShow:1,
+  //   centerMode:true,
+  // })
+})
