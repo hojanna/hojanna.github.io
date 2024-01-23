4ad9250c5ddd939773f9521f5830e23b40c7c69a
hojanna <seungho2528@naver.com>
2024-01-23 17:31:25 +0900

    수정

--- practice/js/custom.js
+++ practice/js/custom.js
@@ -12,11 +12,38 @@ $(document).ready(function(){
   /* brunch recommend */
   $('.brunch_recommend.sliders').slick({
     centerMode: true,
-    centerPadding: '60px',
     slidesToShow: 3,
     dots:true,
     infinite:false,
     variableWidth:true,
     arrows:true,
+  })
+  /* brunch_article */
+  $('.brunch_articles_sliders').slick({
+    cnterMode:true,
+    slidesToShow :3,
+    dots:false,
+    infinite:false,
+    variableWidth:true,
+    arrows:true,
+  })
+  /* tab메뉴 */
+  $('.brunch_weekly_tab').click(function(){
+    $(this).addClass('active')
+    $(this).siblings('.brunch_weekly_tab').removeClass('active')
+    $('.brunch_weekly_content').removeClass('active')
+    $('#' + $(this).attr('data-alt')).addClass('active')
+  })
+  $('.brunch_weekly_sort_tab').click(function(){
+    $(this).addClass('active')
+    $(this).siblings('.brunch_weekly_sort_tab').removeClass('active')
+    $('#' + $(this).attr('data-alt')).siblings('.brunch_weekly.sort_contents').removeClass('active')
+    $('#' + $(this).attr('data-alt')).addClass('active')
+  })
+  $('.brunch_writer_tab').click(function(){
+    $(this).addClass('active')
+    $(this).siblings('.brunch_writer_tab').removeClass('active')
+    $('.brunch_writer_content').removeClass('active')
+    $('#' + $(this).attr('data-alt')).addClass('active')
   })
 })
